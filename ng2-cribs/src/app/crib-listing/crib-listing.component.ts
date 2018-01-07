5844afb413b18638b5e00f3c1cde7e82ea749bbf
Ayebo80 <patrickmaurer@gmx.de>
2018-01-07 19:28:54 +0100

    implemented AngularCLI
    implemented AngularCLI

--- ng2-cribs/src/app/crib-listing/crib-listing.component.ts
+++ ng2-cribs/src/app/crib-listing/crib-listing.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
-// Component-Decorator @Component for class CribListingComponent
 @Component({
   selector: 'app-crib-listing',
   templateUrl: './crib-listing.component.html',
   styleUrls: ['./crib-listing.component.css']
 })
-// OnInit: do something whenever this component starts up
 export class CribListingComponent implements OnInit {
 
   constructor() { }
