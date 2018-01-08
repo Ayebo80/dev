3e269eac4c52244ba47eed0faa94a6fa948da129
Ayebo80 <patrickmaurer@gmx.de>
2018-01-08 18:43:01 +0100

    Angular2+ Vid6
    Angular2+ Vid6

--- ng2-cribs/src/app/crib-listing/crib-listing.component.ts
+++ ng2-cribs/src/app/crib-listing/crib-listing.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { cribs } from './../data/cribs';
 
 @Component({
+  // app-crib-listing is a CHILD of our top Component
   selector: 'app-crib-listing',
   templateUrl: './crib-listing.component.html',
   styleUrls: ['./crib-listing.component.css']
 })
 export class CribListingComponent implements OnInit {
+
+  cribs: Array<any> = cribs;
 
   constructor() { }
 
